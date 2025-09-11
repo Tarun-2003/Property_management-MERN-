b81369a6a41d7ea654c3e043e37aaa7ac6927200
Tarun-2003 <113117993+Tarun-2003@users.noreply.github.com>
2025-09-11 23:26:07 +0100

    Added backend (server) with Express + MongoDB setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from 'cors';
+import connectDB from "./mongodb/connect.js";
+import userRouter from "./routes/user.routes";
+import propertyRouter from "./routes/property.routes.js"
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit:'50mb'}))
+
+app.get("/", (req,res)=>{
+    res.send("hello world") 
+})
+
+const startserver = async () =>{
+    try{
+        //connect to database
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080,()=>console.log("Server has started at 8080"))
+
+    }catch (error){
+        console.log(error);
+    }
+}
+startserver();
