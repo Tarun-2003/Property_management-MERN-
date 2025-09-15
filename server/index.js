d90227bfdd5d332c968c61f7fabb2a5a2c06acee
Tarun-2003 <113117993+Tarun-2003@users.noreply.github.com>
2025-09-15 10:05:37 +0100

    Setup property, user routes and controllers

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from 'cors';
 import connectDB from "./mongodb/connect.js";
-import userRouter from "./routes/user.routes";
+import userRouter from "./routes/user.routes.js";
 import propertyRouter from "./routes/property.routes.js"
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({limit:'50mb'}))
+
+app.use('/api/v1/users/', userRouter)
+
+app.use('/api/v1/properties/', propertyRouter)
+
 
 app.get("/", (req,res)=>{
     res.send("hello world") 
