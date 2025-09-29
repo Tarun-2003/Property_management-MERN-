c83b77a074d6d267299c018097428ea91c4d9c27
Tarun-2003 <113117993+Tarun-2003@users.noreply.github.com>
2025-09-29 18:44:47 +0100

    Add Agents list & Agent profile pages, refine profile fixes, project ~90% complete

--- client/src/pages/my-profile.tsx
+++ client/src/pages/my-profile.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { useGetIdentity, useOne } from "@refinedev/core";
+import { Profile } from "../components";
 
 const MyProfile = () => {
-  return (
-    <div>
-      myProfile
-    </div>
-  )
-}
+  const { data: user } = useGetIdentity({
+    v3LegacyAuthProviderCompatible: true,
+  });
 
-export default MyProfile
+  console.log("user from getIdentity:", user);
+
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: user?._id,   // 👈 use id instead of userid
+    queryOptions: {
+      enabled: !!user?._id, // 👈 only fetch when id exists
+    },
+  });
+
+  const myProfile = data?.data ?? {};
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return (
+    <Profile
+      type="My"
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties ?? []}
+    />
+  );
+};
+
+export default MyProfile;
